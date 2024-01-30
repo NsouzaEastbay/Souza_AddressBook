8de198044cd2f3c04aa07a25368a71fec11fd354
Me <142674441+NsouzaEastbay@users.noreply.github.com>
2024-01-30 10:08:39 -0800

    Added Code for second exercise
    Added a menu of sorts that allows repeated entries on the same instance.
    Added the ability to save entered address data into a file that will eventually be able to be read.
    Added FileIO file for later use in reading the file
    
    Repeat of prior commit with the right file upload this time

--- AddressBookApplication/src/AddressBook/AddressBookApplication.java
+++ AddressBookApplication/src/AddressBook/AddressBookApplication.java
@@ -1,18 +1,20 @@
 package AddressBook;
-
 /**
  * Main File for the Address Book Application
  */
 
 public class AddressBookApplication {
     public static void main(String[] args) {
-        Menu.prompt_FirstName();
-        Menu.prompt_LastName();
-        Menu.prompt_Street();
-        Menu.prompt_City();
-        Menu.prompt_State();
-        Menu.prompt_Zip();
-        Menu.prompt_Telephone();
-        Menu.prompt_Email();
+        while(Menu.menu_Prompt()){
+            Menu.prompt_FirstName();
+            Menu.prompt_LastName();
+            Menu.prompt_Street();
+            Menu.prompt_City();
+            Menu.prompt_State();
+            Menu.prompt_Zip();
+            Menu.prompt_Telephone();
+            Menu.prompt_Email();
+        }
+        System.exit(0);
     }
 }
--- AddressBookApplication/src/AddressBook/Menu.java
+++ AddressBookApplication/src/AddressBook/Menu.java
@@ -1,41 +1,76 @@
 package AddressBook;
 
+import java.util.Objects;
+import java.util.Scanner;
+
 /**
  * Menu Class for the Address Book Application which
  * handles the prompts that ask the user to input
  * information they want into their address book.
  */
 public class Menu {
-    public static String prompt_FirstName(){
+    /*Though this can be done easier this asks the user for each prompt individually
+      then writes each of them to the file followed by a common for all put the last
+       which is replaced by a new line to divide entries in the book*/
+    public static void prompt_FirstName(){
         System.out.println("First Name:");
-        return "";
+        Scanner firstName = new Scanner(System.in);
+        String FirstName = firstName.nextLine();
+        FileIO.WriteFile(FirstName);
+        FileIO.WriteFile(",");
     }
-    public static String prompt_LastName(){
+    public static void prompt_LastName(){
         System.out.println("Last Name:");
-        return "";
+        Scanner lastName = new Scanner(System.in);
+        String LastName = lastName.nextLine();
+        FileIO.WriteFile(LastName);
+        FileIO.WriteFile(",");
     }
-    public static String prompt_Street(){
+    public static void prompt_Street(){
         System.out.println("Street Name:");
-        return "";
+        Scanner streetName = new Scanner(System.in);
+        String StreetName = streetName.nextLine();
+        FileIO.WriteFile(StreetName);
+        FileIO.WriteFile(",");
     }
-    public static String prompt_City(){
+    public static void prompt_City(){
         System.out.println("City Name:");
-        return "";
+        Scanner cityName = new Scanner(System.in);
+        String CityName = cityName.nextLine();
+        FileIO.WriteFile(CityName);
+        FileIO.WriteFile(",");
     }
-    public static String prompt_State(){
+    public static void prompt_State(){
         System.out.println("State Name:");
-        return "";
+        Scanner stateName = new Scanner(System.in);
+        String StateName = stateName.nextLine();
+        FileIO.WriteFile(StateName);
+        FileIO.WriteFile(",");
     }
-    public static String prompt_Zip(){
+    public static void prompt_Zip(){
         System.out.println("Zip Code:");
-        return "";
+        Scanner zipCode = new Scanner(System.in);
+        String ZipCode = zipCode.nextLine();
+        FileIO.WriteFile(ZipCode);
+        FileIO.WriteFile(",");
     }
-    public static String prompt_Telephone(){
+    public static void prompt_Telephone(){
         System.out.println("Telephone Number:");
-        return "";
+        Scanner telephoneNumber = new Scanner(System.in);
+        String TelephoneNumber = telephoneNumber.nextLine();
+        FileIO.WriteFile(TelephoneNumber);
+        FileIO.WriteFile(",");
     }
-    public static String prompt_Email(){
+    public static void prompt_Email(){
         System.out.println("Email Address:");
-        return "";
+        Scanner emailAddress = new Scanner(System.in);
+        String EmailAddress = emailAddress.nextLine();
+        FileIO.WriteFile(EmailAddress);
+        FileIO.WriteFile("\n");
+    }
+    public static boolean menu_Prompt(){
+        System.out.println("Main Menu");
+        Scanner command = new Scanner(System.in);
+        return Objects.equals(command.nextLine(), "R");
     }
 }
