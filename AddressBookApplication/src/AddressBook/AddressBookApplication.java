686e4842a26c291f099aaaaf97905d62b5bed05b
Nickolys <nsouza5@horizon.csueastbay.edu>
2024-02-13 15:01:27 -0800

    Main Menu part one Updated the Menu class to display a prompt for users asking them what feature they would like to use based on a letter input. Also allows for quitting though any input at this time.

--- AddressBookApplication/src/AddressBook/AddressBookApplication.java
+++ AddressBookApplication/src/AddressBook/AddressBookApplication.java
@@ -1,41 +1,15 @@
 package AddressBook;
 
 import AddressBook.data.AddressEntry;
-
 /**
  * Main File for the Address Book Application responsible for taking the inputs
  */
 
 public class AddressBookApplication {
     public static void main(String[] args) {
-            AddressBook addressBook = new AddressBook();
-            int i = 1;
-            while (i <= 2) {
-                    AddressEntry Entry = new AddressEntry();
-                    if (i == 1) {
-                            addressBook.addressEntryList();
-                            Entry.setFirstName("Test");
-                            Entry.setLastName("test");
-                            Entry.setCity("test");
-                            Entry.setEmail("Test@gmail.com");
-                            Entry.setState("Test");
-                            Entry.setPhone("111-111-1111");
-                            Entry.setZip(12345);
-                            Entry.setStreet("Test");
-                            addressBook.add(Entry);
-                            i++;
-                    }
-                    else {
-                            Entry.setFirstName("Not a Test");
-                            addressBook.add(Entry);
-                            i++;
-                    }
-            }
-
-
-            AddressEntry[] addressEntries = addressBook.list();
-            for (int k = 0; k < addressEntries.length; k++){
-                System.out.println(addressEntries[k].toString());
-            }
+        AddressBook addressBook = new AddressBook();
+        AddressEntry Entry = new AddressEntry();
+        Menu Menu = new Menu();
+        Menu.MainMenu();
     }
 }
--- AddressBookApplication/src/AddressBook/Menu.java
+++ AddressBookApplication/src/AddressBook/Menu.java
@@ -1,5 +1,28 @@
 package AddressBook;
 
-public class Menu {
+import java.util.Scanner;
 
+public class Menu {
+    String command;
+    boolean quit = true;
+    public void MainMenu(){
+
+        while (quit) {
+            System.out.println("Main Menu");
+            System.out.println("a) Load From File");
+            System.out.println("b) Add New Entry");
+            System.out.println("c) Remove Entry");
+            System.out.println("d) Find Entry");
+            System.out.println("e) List of Entries");
+            System.out.println("f) Quit");
+            Scanner scanner = new Scanner(System.in);
+            command = scanner.nextLine();
+            if (command.equals("Test")){
+                System.out.println("It Works");
+            }
+            else{
+                quit = false;
+            }
+        }
+    }
 }
