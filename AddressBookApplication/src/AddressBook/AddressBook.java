be86f4bf751f964717f7181b2654dbb1de71ff85
Nickolys <nsouza5@horizon.csueastbay.edu>
2024-02-18 14:26:04 -0800

    First Part of File IO -Created readFromFile method which will eventually read all the entries from a file and add them to list

--- AddressBookApplication/src/AddressBook/AddressBook.java
+++ AddressBookApplication/src/AddressBook/AddressBook.java
@@ -2,6 +2,7 @@ package AddressBook;
 
 import AddressBook.data.AddressEntry;
 import java.util.*;
+import java.io.*;
 
 /**
  * The class responsible for reading and writing the data for each entry.
@@ -19,5 +20,26 @@ public class AddressBook {
         this.addressEntryList.toArray(completeList);
         //Arrays.sort(completeList);
         return completeList;
+    }
+    public void readFromFile(String filename){
+        try {
+            File File = new File(filename);
+            Scanner readFile = new Scanner(File);
+            while (readFile.hasNextLine()){
+                AddressEntry Entry = new AddressEntry();
+                Entry.setFirstName(readFile.nextLine());
+                Entry.setLastName(readFile.nextLine());
+                Entry.setStreet(readFile.nextLine());
+                Entry.setCity(readFile.nextLine());
+                Entry.setState(readFile.nextLine());
+                Entry.setPhone(readFile.nextLine());
+                Entry.setEmail(readFile.nextLine());
+                Entry.setZip(readFile.nextInt());
+                add(Entry);
+            }
+        }
+        catch (FileNotFoundException e){
+            System.out.println("File Not Found");
+        }
     }
 }
